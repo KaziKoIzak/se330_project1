7696a3dd3b4b20d844f6473dd3d3b5e05e5c7414
KaziKoIzak <izak.benitezlopez@jacks.sdstate.edu>
2024-03-05 23:36:44 -0600

    lkjsadf

--- lib/pages/white_wine_menu.dart
+++ lib/pages/white_wine_menu.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'about_us_screen.dart';
 import 'checkout_screen.dart';
 import 'menu_screen.dart';
 import 'rooms_screen.dart';
-import 'banner.dart';
+import '../widgets/banner.dart';
 import '../data/selected_items_provider.dart';
 import '../data/items.dart';
 
@@ -75,157 +74,156 @@ class WhiteWineScreen extends StatelessWidget {
             ),
           ),
           Center(
-              child: Container(
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 150, vertical: 10),
-                height: 400,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                      Expanded(
-                        child: Column(
-                        children: [
-                          const Expanded(
-                            child: Center(
-                              child: Text(
-                                'Chardonay',
-                                style: TextStyle(
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold,
-                                ),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 150, vertical: 10),
+              height: 400,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        const Expanded(
+                          child: Center(
+                            child: Text(
+                              'Chardonay',
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
-                          Expanded(
-                            child: ElevatedButton(
+                        ),
+                        Expanded(
+                          child: ElevatedButton(
                             onPressed: () {
-                            // SelectedItemsProvider selectedItemsProvider =
-                            // Provider.of<SelectedItemsProvider>(
-                            //   context,
-                            //   listen: false);
-                            
-                            // String 'Chardonay' = selectedWine;
-                            // WineDetails currentWineDetails =
-                            //   wineDetailsMap['Chardonay']!;
+                              // SelectedItemsProvider selectedItemsProvider =
+                              // Provider.of<SelectedItemsProvider>(
+                              //   context,
+                              //   listen: false);
 
-                            //   selectedItemsProvider.addItem(Item(
-                            //     itemType: ItemType.wine,
-                            //     name: 'Chardonay',
-                            //     quantity: 1,
-                            //     price: currentWineDetails.price,
-                            //   ));
+                              // String 'Chardonay' = selectedWine;
+                              // WineDetails currentWineDetails =
+                              //   wineDetailsMap['Chardonay']!;
+
+                              //   selectedItemsProvider.addItem(Item(
+                              //     itemType: ItemType.wine,
+                              //     name: 'Chardonay',
+                              //     quantity: 1,
+                              //     price: currentWineDetails.price,
+                              //   ));
                             },
                             child: const Text('Add To Checkout'),
                           ),
+                        ),
+                        Expanded(
+                          child: Image.asset(
+                            'assets/images/905.jpg',
+                            width: 250,
+                            height: 250,
                           ),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/images/905.jpg',
-                              width: 250,
-                              height: 250,
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(width: 10.0),
-                    Expanded(
-                        child: Column(
-                        children: [
-                          const Expanded(
-                            child: Center(
-                              child: Text(
-                                'Pinot Grigio',
-                                style: TextStyle(
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                  ),
+                  const SizedBox(width: 10.0),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        const Expanded(
+                          child: Center(
+                            child: Text(
+                              'Pinot Grigio',
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
-                          Expanded(
-                            child: ElevatedButton(
+                        ),
+                        Expanded(
+                          child: ElevatedButton(
                             onPressed: () {
-                            // SelectedItemsProvider selectedItemsProvider =
-                            // Provider.of<SelectedItemsProvider>(
-                            //   context,
-                            //   listen: false);
-                            
-                            // String 'Pinot Grigio' = selectedWine;
-                            // WineDetails currentWineDetails =
-                            //   wineDetailsMap['Pinot Grigio']!;
+                              // SelectedItemsProvider selectedItemsProvider =
+                              // Provider.of<SelectedItemsProvider>(
+                              //   context,
+                              //   listen: false);
 
-                            //   selectedItemsProvider.addItem(Item(
-                            //     itemType: ItemType.wine,
-                            //     name: 'Pinot Grigio',
-                            //     quantity: 1,
-                            //     price: currentWineDetails.price,
-                            //  ));
+                              // String 'Pinot Grigio' = selectedWine;
+                              // WineDetails currentWineDetails =
+                              //   wineDetailsMap['Pinot Grigio']!;
+
+                              //   selectedItemsProvider.addItem(Item(
+                              //     itemType: ItemType.wine,
+                              //     name: 'Pinot Grigio',
+                              //     quantity: 1,
+                              //     price: currentWineDetails.price,
+                              //  ));
                             },
                             child: const Text('Add To Checkout'),
                           ),
+                        ),
+                        Expanded(
+                          child: Image.asset(
+                            'assets/images/679.jpg',
+                            width: 250,
+                            height: 250,
                           ),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/images/679.jpg',
-                              width: 250,
-                              height: 250,
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(width: 10.0),
-                    Expanded(
-                        child: Column(
-                        children: [
-                          const Expanded(
-                            child: Center(
-                              child: Text(
-                                'Sauvignon Blanc',
-                                style: TextStyle(
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                  ),
+                  const SizedBox(width: 10.0),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        const Expanded(
+                          child: Center(
+                            child: Text(
+                              'Sauvignon Blanc',
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
-                          Expanded(
-                            child: ElevatedButton(
+                        ),
+                        Expanded(
+                          child: ElevatedButton(
                             onPressed: () {
-                            // SelectedItemsProvider selectedItemsProvider =
-                            // Provider.of<SelectedItemsProvider>(
-                            //   context,
-                            //   listen: false);
-                            
-                            // String 'Sauvignon Blancé ' = selectedWine;
-                            // WineDetails currentWineDetails =
-                            //   wineDetailsMap['Sauvignon Blanc ']!;
+                              // SelectedItemsProvider selectedItemsProvider =
+                              // Provider.of<SelectedItemsProvider>(
+                              //   context,
+                              //   listen: false);
 
-                            //   selectedItemsProvider.addItem(Item(
-                            //     itemType: ItemType.wine,
-                            //     name: 'Sauvignon Blanc ',
-                            //     quantity: 1,
-                            //     price: currentWineDetails.price,
-                             // ));
+                              // String 'Sauvignon Blancé ' = selectedWine;
+                              // WineDetails currentWineDetails =
+                              //   wineDetailsMap['Sauvignon Blanc ']!;
+
+                              //   selectedItemsProvider.addItem(Item(
+                              //     itemType: ItemType.wine,
+                              //     name: 'Sauvignon Blanc ',
+                              //     quantity: 1,
+                              //     price: currentWineDetails.price,
+                              // ));
                             },
                             child: const Text('Add To Checkout'),
                           ),
+                        ),
+                        Expanded(
+                          child: Image.asset(
+                            'assets/images/906.jpg',
+                            width: 250,
+                            height: 250,
                           ),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/images/906.jpg',
-                              width: 250,
-                              height: 250,
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
+          ),
         ],
       ),
     );

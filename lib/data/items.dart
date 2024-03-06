e34b34477074a300bc245ef6baec1cf188136ce4
KaziKoIzak <izak.benitezlopez@jacks.sdstate.edu>
2024-03-05 23:32:14 -0600

    Denomition

--- lib/data/items.dart
+++ lib/data/items.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 enum ItemType {
   room,
   wine,
--- lib/pages/red_wine_menu.dart
+++ lib/pages/red_wine_menu.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'about_us_screen.dart';
 import 'checkout_screen.dart';
 import 'menu_screen.dart';
 import 'rooms_screen.dart';
-import 'banner.dart';
+import '../widgets/banner.dart';
 import '../data/selected_items_provider.dart';
 import '../data/items.dart';
 
@@ -45,18 +44,18 @@ class RedWineScreen extends StatelessWidget {
               children: [
                 Expanded(
                   child: Image.asset(
-                    'assets/images/567.jpg', // Replace with your left image asset
-                    width: 200.0, // Adjust width as needed
-                    height: 250.0, // Adjust height as needed
+                    'assets/images/567.jpg',
+                    width: 200.0,
+                    height: 250.0,
                   ),
-                ), // Adjust spacing between images
+                ),
                 Expanded(
                   child: Center(
                     child: Text(
                       'Red Wines',
                       style: GoogleFonts.pacifico(
                         textStyle: const TextStyle(
-                          fontSize: 50, // Set the font size as needed
+                          fontSize: 50,
                           fontWeight: FontWeight.bold,
                           fontStyle: FontStyle.italic,
                         ),
@@ -66,168 +65,97 @@ class RedWineScreen extends StatelessWidget {
                 ),
                 Expanded(
                   child: Image.asset(
-                    'assets/images/567.jpg', // Replace with your right image asset
-                    width: 200.0, // Adjust width as needed
-                    height: 250.0, // Adjust height as needed
+                    'assets/images/567.jpg',
+                    width: 200.0,
+                    height: 250.0,
                   ),
                 ),
               ],
             ),
           ),
           Center(
-              child: Container(
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 150, vertical: 10),
-                height: 400,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                      Expanded(
-                        child: Column(
-                        children: [
-                          const Expanded(
-                            child: Center(
-                              child: Text(
-                                'Pinot Noir',
-                                style: TextStyle(
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: ElevatedButton(
-                            onPressed: () {
-                            // SelectedItemsProvider selectedItemsProvider =
-                            // Provider.of<SelectedItemsProvider>(
-                            //   context,
-                            //   listen: false);
-                            
-                            // String 'Pinot Noir' = selectedWine;
-                            // WineDetails currentWineDetails =
-                            //   wineDetailsMap['Pinot Noir']!;
-
-                            //   selectedItemsProvider.addItem(Item(
-                            //     itemType: ItemType.wine,
-                            //     name: 'Pinot Noir',
-                            //     quantity: 1,
-                            //     price: currentWineDetails.price,
-                            //   ));
-                            },
-                            child: const Text('Add To Checkout'),
-                          ),
-                          ),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/images/902.jpg',
-                              width: 250,
-                              height: 250,
-                            ),
-                          ),
-                        ],
-                      ),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 150, vertical: 10),
+              height: 400,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: buildWineColumn(
+                      'Pinot Noir',
+                      'assets/images/902.jpg',
+                      845.23,
+                      context,
                     ),
-                    const SizedBox(width: 10.0),
-                    Expanded(
-                        child: Column(
-                        children: [
-                          const Expanded(
-                            child: Center(
-                              child: Text(
-                                'Malbec',
-                                style: TextStyle(
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: ElevatedButton(
-                            onPressed: () {
-                            // SelectedItemsProvider selectedItemsProvider =
-                            // Provider.of<SelectedItemsProvider>(
-                            //   context,
-                            //   listen: false);
-                            
-                            // String 'Malbec' = selectedWine;
-                            // WineDetails currentWineDetails =
-                            //   wineDetailsMap['Malbec']!;
-
-                            //   selectedItemsProvider.addItem(Item(
-                            //     itemType: ItemType.wine,
-                            //     name: 'Malbec',
-                            //     quantity: 1,
-                            //     price: currentWineDetails.price,
-                            //  ));
-                            },
-                            child: const Text('Add To Checkout'),
-                          ),
-                          ),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/images/903.jpg',
-                              width: 250,
-                              height: 250,
-                            ),
-                          ),
-                        ],
-                      ),
+                  ),
+                  const SizedBox(width: 10.0),
+                  Expanded(
+                    child: buildWineColumn(
+                      'Malbec',
+                      'assets/images/903.jpg',
+                      123.23,
+                      context,
                     ),
-                    const SizedBox(width: 10.0),
-                    Expanded(
-                        child: Column(
-                        children: [
-                          const Expanded(
-                            child: Center(
-                              child: Text(
-                                'Provence Rosé ',
-                                style: TextStyle(
-                                  fontSize: 40,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: ElevatedButton(
-                            onPressed: () {
-                            // SelectedItemsProvider selectedItemsProvider =
-                            // Provider.of<SelectedItemsProvider>(
-                            //   context,
-                            //   listen: false);
-                            
-                            // String 'Provence Rosé ' = selectedWine;
-                            // WineDetails currentWineDetails =
-                            //   wineDetailsMap['Provence Rosé ']!;
-
-                            //   selectedItemsProvider.addItem(Item(
-                            //     itemType: ItemType.wine,
-                            //     name: 'Provence Rosé ',
-                            //     quantity: 1,
-                            //     price: currentWineDetails.price,
-                             // ));
-                            },
-                            child: const Text('Add To Checkout'),
-                          ),
-                          ),
-                          Expanded(
-                            child: Image.asset(
-                              'assets/images/904.jpg',
-                              width: 250,
-                              height: 250,
-                            ),
-                          ),
-                        ],
-                      ),
+                  ),
+                  const SizedBox(width: 10.0),
+                  Expanded(
+                    child: buildWineColumn(
+                      'Provence Rosé',
+                      'assets/images/904.jpg',
+                      234.45,
+                      context,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
+          ),
         ],
       ),
+    );
+  }
+
+  // Helper method to build each wine column
+  Widget buildWineColumn(String wineName, String imagePath, double priceHeight,
+      BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            // When the button is pressed, add the wine item to selectedItems
+            Provider.of<SelectedItemsProvider>(context, listen: false).addItem(
+              Item(
+                  itemType: ItemType.wine,
+                  name: wineName,
+                  quantity: 1,
+                  price: priceHeight // Set the price for the wine item,
+                  ),
+            );
+          },
+          child: Text(
+            'Select $wineName',
+            style: const TextStyle(fontSize: 16),
+          ),
+        ),
+        Expanded(
+          child: Center(
+            child: Text(
+              wineName,
+              style: const TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Image.asset(
+            imagePath,
+            width: 250,
+            height: 250,
+          ),
+        ),
+      ],
     );
   }
 }

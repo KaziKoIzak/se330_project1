4a97554984e0ae74552112945f595876c87bac04
KaziKoIzak <izak.benitezlopez@jacks.sdstate.edu>
2024-03-05 19:49:35 -0600

    Endings

--- lib/widgets/checkout_screen.dart
+++ lib/widgets/checkout_screen.dart
@@ -4,6 +4,8 @@ import 'about_us_screen.dart';
 import 'menu_screen.dart';
 import 'rooms_screen.dart';
 import 'banner.dart';
+import 'package:provider/provider.dart';
+import '../data/selected_items_provider.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({Key? key}) : super(key: key);
@@ -43,7 +45,7 @@ class CheckoutScreen extends StatelessWidget {
                   child: Image.asset(
                     'assets/images/567.jpg',
                     width: 200.0,
-                    height: 250.0,
+                    height: 200.0,
                   ),
                 ),
                 Expanded(
@@ -64,12 +66,152 @@ class CheckoutScreen extends StatelessWidget {
                   child: Image.asset(
                     'assets/images/567.jpg',
                     width: 200.0,
-                    height: 250.0,
+                    height: 200.0,
                   ),
                 ),
               ],
             ),
           ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              // First Child - Large Vertical Box
+              Expanded(
+                child: Container(
+                  height: 500,
+                  margin: const EdgeInsets.only(left: 60, right: 60),
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(
+                      color: Colors.black, // Border color
+                      width: 2.0, // Border width
+                    ),
+                    borderRadius: BorderRadius.circular(12.0), // Border radius
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2), // Shadow color
+                        spreadRadius: 2.0, // Spread radius
+                        blurRadius: 5.0, // Blur radius
+                        offset: const Offset(0, 2), // Offset (x, y)
+                      ),
+                    ],
+                  ),
+                  child: Consumer<SelectedItemsProvider>(
+                    builder: (context, selectedItemsProvider, _) {
+                      // Use the selectedItemsProvider.selectedItems list
+                      return ListView.builder(
+                        itemCount: selectedItemsProvider.selectedItems.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          // Replace with your widget for each selected item
+                          return ListTile(
+                            title: Text(selectedItemsProvider
+                                .selectedItems[index].name),
+                            // Additional details or actions for each item can be added here
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+              // Second Child - Column with 5 Rows
+              Expanded(
+                child: Column(
+                  children: [
+                    // First Row
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            color: Colors.blue,
+                            height: 50.0,
+                            // Replace with content for the first child in the first row
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.green,
+                            height: 50.0,
+                            // Replace with content for the second child in the first row
+                          ),
+                        ),
+                      ],
+                    ),
+                    // Second Row
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            color: Colors.orange,
+                            height: 50.0,
+                            // Replace with content for the first child in the second row
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.red,
+                            height: 50.0,
+                            // Replace with content for the second child in the second row
+                          ),
+                        ),
+                      ],
+                    ),
+                    // Third Row
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            color: Colors.purple,
+                            height: 50.0,
+                            // Replace with content for the first child in the third row
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.yellow,
+                            height: 50.0,
+                            // Replace with content for the second child in the third row
+                          ),
+                        ),
+                      ],
+                    ),
+                    // Fourth Row
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            color: Colors.teal,
+                            height: 50.0,
+                            // Replace with content for the first child in the fourth row
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.brown,
+                            height: 50.0,
+                            // Replace with content for the second child in the fourth row
+                          ),
+                        ),
+                      ],
+                    ),
+                    // Fifth Row (Single Child)
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            color: Colors.grey,
+                            height: 50.0,
+                            // Replace with content for the single child in the fifth row
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );

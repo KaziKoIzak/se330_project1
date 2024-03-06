97125743a59d3487f8eaec3fb309618fa4e481c9
RBTraeder <117694074+RBTraeder@users.noreply.github.com>
2024-03-05 23:28:57 -0600

    Update white_wine_menu.dart

--- lib/pages/white_wine_menu.dart
+++ lib/pages/white_wine_menu.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'about_us_screen.dart';
 import 'checkout_screen.dart';
 import 'menu_screen.dart';
 import 'rooms_screen.dart';
-import '../widgets/banner.dart';
+import 'banner.dart';
+import '../data/selected_items_provider.dart';
+import '../data/items.dart';
 
 class WhiteWineScreen extends StatelessWidget {
   const WhiteWineScreen({Key? key}) : super(key: key);
@@ -70,6 +74,158 @@ class WhiteWineScreen extends StatelessWidget {
               ],
             ),
           ),
+          Center(
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 150, vertical: 10),
+                height: 400,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                      Expanded(
+                        child: Column(
+                        children: [
+                          const Expanded(
+                            child: Center(
+                              child: Text(
+                                'Chardonay',
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: ElevatedButton(
+                            onPressed: () {
+                            // SelectedItemsProvider selectedItemsProvider =
+                            // Provider.of<SelectedItemsProvider>(
+                            //   context,
+                            //   listen: false);
+                            
+                            // String 'Chardonay' = selectedWine;
+                            // WineDetails currentWineDetails =
+                            //   wineDetailsMap['Chardonay']!;
+
+                            //   selectedItemsProvider.addItem(Item(
+                            //     itemType: ItemType.wine,
+                            //     name: 'Chardonay',
+                            //     quantity: 1,
+                            //     price: currentWineDetails.price,
+                            //   ));
+                            },
+                            child: const Text('Add To Checkout'),
+                          ),
+                          ),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/images/905.jpg',
+                              width: 250,
+                              height: 250,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 10.0),
+                    Expanded(
+                        child: Column(
+                        children: [
+                          const Expanded(
+                            child: Center(
+                              child: Text(
+                                'Pinot Grigio',
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: ElevatedButton(
+                            onPressed: () {
+                            // SelectedItemsProvider selectedItemsProvider =
+                            // Provider.of<SelectedItemsProvider>(
+                            //   context,
+                            //   listen: false);
+                            
+                            // String 'Pinot Grigio' = selectedWine;
+                            // WineDetails currentWineDetails =
+                            //   wineDetailsMap['Pinot Grigio']!;
+
+                            //   selectedItemsProvider.addItem(Item(
+                            //     itemType: ItemType.wine,
+                            //     name: 'Pinot Grigio',
+                            //     quantity: 1,
+                            //     price: currentWineDetails.price,
+                            //  ));
+                            },
+                            child: const Text('Add To Checkout'),
+                          ),
+                          ),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/images/679.jpg',
+                              width: 250,
+                              height: 250,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 10.0),
+                    Expanded(
+                        child: Column(
+                        children: [
+                          const Expanded(
+                            child: Center(
+                              child: Text(
+                                'Sauvignon Blanc',
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: ElevatedButton(
+                            onPressed: () {
+                            // SelectedItemsProvider selectedItemsProvider =
+                            // Provider.of<SelectedItemsProvider>(
+                            //   context,
+                            //   listen: false);
+                            
+                            // String 'Sauvignon Blancé ' = selectedWine;
+                            // WineDetails currentWineDetails =
+                            //   wineDetailsMap['Sauvignon Blanc ']!;
+
+                            //   selectedItemsProvider.addItem(Item(
+                            //     itemType: ItemType.wine,
+                            //     name: 'Sauvignon Blanc ',
+                            //     quantity: 1,
+                            //     price: currentWineDetails.price,
+                             // ));
+                            },
+                            child: const Text('Add To Checkout'),
+                          ),
+                          ),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/images/906.jpg',
+                              width: 250,
+                              height: 250,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
         ],
       ),
     );

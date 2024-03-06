fa743538df7e5bdd1333b1c5174819572e849ddc
KaziKoIzak <izak.benitezlopez@jacks.sdstate.edu>
2024-03-05 22:05:50 -0600

    Difference

--- lib/widgets/wine_menu.dart
+++ lib/widgets/wine_menu.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:se330_project1/widgets/red_wine_menu.dart';
 import 'package:se330_project1/widgets/white_wine_menu.dart';
-import 'about_us_screen.dart';
 import 'menu_screen.dart';
 import 'checkout_screen.dart';
 import 'rooms_screen.dart';

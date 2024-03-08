86e703582cc2e36dd1cac62bf67e0fb69334df70
RBTraeder <117694074+RBTraeder@users.noreply.github.com>
2024-03-08 08:48:58 -0600

    Update charcuterie_menu.dart

--- lib/pages/charcuterie_menu.dart
+++ lib/pages/charcuterie_menu.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'checkout_screen.dart';
 import 'menu_screen.dart';
 import 'rooms_screen.dart';
+import 'about_us_screen.dart';
 import '../widgets/banner.dart';
 import 'package:provider/provider.dart';
 import '../data/selected_items_provider.dart';
@@ -65,6 +66,12 @@ class _CharcutterieScreenState extends State<CharcutterieMenu> {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => const CheckoutScreen()),
+          );
+        },
+        onAboutUsPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AboutUsScreen()),
           );
         },
       ),
